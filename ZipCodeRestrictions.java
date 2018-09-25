2eaff120eb9be570be453f0beec3596da958f5b1
onkarmjadhav <43520826+onkarmjadhav@users.noreply.github.com>
2018-09-26 00:22:59 +0530

    Updating final code
    Updating final code

--- ZipCodeRestrictions.java
+++ ZipCodeRestrictions.java
@@ -1,86 +1,102 @@
 package com.test;
 
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.concurrent.ConcurrentHashMap;
 
-public class ZipCodeRestrictions {
-	
-	static Map<Integer, Integer> zipMap = new LinkedHashMap<>();
+public class ZipCodeRange {
+
+	static Map<Integer, Integer> zipMap = new TreeMap<>();
 
 	public static void main(String[] args) {
-		String zipCodes = "[94500,94599] [94140,94210] [94210,94500] [94200,94299] [94600,94699] [94650,94689] [94343,94454]";
+		// String zipCodes = "[94500,94599] [94140,94210] [94210,94501]
+		// [94200,94299] [94600,94699] [94650,94689] [94343,94454]";
+		// String zipCodes = "[94133,94133] [94200,94299] [94226,94399]";
+		String zipCodes = "[94133,94133] [94200,94299] [94600,94699]";
 		arrangeZipCodes(zipCodes);
 	}
-	
-	public static void arrangeZipCodes(String zipCodes){
-		
-		String []zipCodeArray = zipCodes.split(" ");
+
+	public static void arrangeZipCodes(String zipCodes) {
+
+		String[] zipCodeArray = zipCodes.trim().split(" ");
 		for (int i = 0; i < zipCodeArray.length; i++) {
 			String zipCodeBoundary = zipCodeArray[i];
-			String zipCodeLower = zipCodeBoundary.substring(1, 6);
-			String zipCodeHigher = zipCodeBoundary.substring(7, 12);
-			
-			validateZipCodes(Integer.parseInt(zipCodeLower), Integer.parseInt(zipCodeHigher));
+			String zipCodeLower = null;
+			String zipCodeHigher = null;
+			try {
+				if (zipCodeBoundary.charAt(0) == '[' && zipCodeBoundary.lastIndexOf('[') == 0
+						&& zipCodeBoundary.charAt(zipCodeBoundary.length() - 1) == ']'
+						&& zipCodeBoundary.lastIndexOf(']') == zipCodeBoundary.length() - 1) {//Validate input
+					
+
+					zipCodeLower = zipCodeBoundary.substring(1, 6);
+					zipCodeHigher = zipCodeBoundary.substring(7, 12);
+
+					validateZipCodes(Integer.parseInt(zipCodeLower), Integer.parseInt(zipCodeHigher));
+
+				} else {
+					System.out.println("Invalid Input");
+				}
+			} catch (StringIndexOutOfBoundsException e) {
+				System.out.println("Invalid Input : String is not valid at Index : " + i);
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid Input");
+			} catch (Exception e) {
+				System.out.println("Invalid Input");
+			}
+
 		}
-		
+
 		displayManipulatedZipCodes();
+
 	}
 
+	/**
+	 * Display final result
+	 */
 	private static void displayManipulatedZipCodes() {
 		Set<Entry<Integer, Integer>> zipEntrySet = zipMap.entrySet();
-		
-		for(Entry<Integer, Integer> entry : zipEntrySet){
+
+		for (Entry<Integer, Integer> entry : zipEntrySet) {
 			System.out.print("[" + entry.getKey() + "," + entry.getValue() + "] ");
 		}
-		
+
 	}
 
+	/**
+	 * This method validate each zipcode with the upper & lower range stored in Map
+	 * @param zipCodeLower
+	 * @param zipCodeHigher
+	 */
 	private static void validateZipCodes(int zipCodeLower, int zipCodeHigher) {
-		
-		if(!zipMap.isEmpty()){
+
+		if (!zipMap.isEmpty()) {
 			Set<Entry<Integer, Integer>> zipEntrySet = zipMap.entrySet();
-			int i = 0;
-			boolean visited = false;
-			for(Entry<Integer, Integer> entry : zipEntrySet){
-				i++;
+			Iterator<Map.Entry<Integer, Integer>> itr = zipEntrySet.iterator();
+			while (itr.hasNext()) {
+				Map.Entry<Integer, Integer> entry = itr.next();
 				int low = entry.getKey();
 				int high = entry.getValue();
-				
-				if(zipCodeLower < low && zipCodeHigher > low && zipCodeHigher < high){
-					zipMap.remove(low);
-					zipMap.put(zipCodeLower, high);
-					visited = true;
-					continue;
-				}else if(zipCodeLower > low && zipCodeHigher < high){
-					visited = true;
-					continue;
-				}else if(zipCodeHigher > high && zipCodeLower > low && zipCodeLower <= high){
-					zipMap.replace(low, zipCodeHigher);
-					visited = true;
-					continue;
-				}else if(zipCodeLower < low && zipCodeHigher > high){
-					zipMap.remove(low);
-					zipMap.put(zipCodeLower, zipCodeHigher);
-					visited = true;
-					continue;
-				}else{
-					if(i == zipMap.size() && !visited){
-						zipMap.put(zipCodeLower, zipCodeHigher);
-					}
+				if (zipCodeLower >= low && zipCodeLower <= high) {
+					zipCodeLower = low;
+					if (zipCodeHigher < high)
+						zipCodeHigher = high;
+				} else if (zipCodeHigher >= low && zipCodeHigher <= high) {
+					itr.remove();
+					zipCodeHigher = high;
+				} else if (zipCodeLower < low) {
+					itr.remove();
+					if (zipCodeHigher < high)
+						zipCodeHigher = high;
 				}
 			}
-			
-		}else{
+
+			zipMap.put(zipCodeLower, zipCodeHigher);
+
+		} else {
 			zipMap.put(zipCodeLower, zipCodeHigher);
 		}
-		
-		
 	}
-
 }
